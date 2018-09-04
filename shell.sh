f16ed76fb174a0720ffd22cdd69667ff57f873e0
ahmednet <ahmednet03@gmail.com>
2018-09-04 19:17:02 +0200

    Operations

--- shell.sh
+++ shell.sh
@@ -1,11 +1,16 @@
 #! /usr/bin/bash
 # ===start code===
-#=== arguments
-# $# length
-# $@ array of argumest
-for i in $@
-do
-	echo "my name is $i"
-done
+#===Operations===
+
+# && / -a = and
+# || / -o = or
+echo -e 'enter your age to know \c'
+read age
+if [[ $age -lt 50 && $age -gt 20 ]]
+then
+	echo 'your age is okey'
+else
+	echo 'your age is bad'
+fi
 # ===end code===
 
