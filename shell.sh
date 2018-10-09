b84514b6b188c02ab1a583097175f4698d7def9f
ahmednet <ahmednet03@gmail.com>
2018-10-09 09:22:55 +0200

    while

--- shell.sh
+++ shell.sh
@@ -4,13 +4,19 @@
 
 # && / -a = and
 # || / -o = or
-echo -e 'enter your age to know \c'
-read age
-if [[ $age -lt 50 && $age -gt 20 ]]
-then
-	echo 'your age is okey'
-else
-	echo 'your age is bad'
-fi
+# echo -e 'enter your age to know \c'
+# read age
+# if [[ $age -lt 50 && $age -gt 20 ]]
+# then
+# 	echo 'your age is okey'
+# else
+# 	echo 'your age is bad'
+# fi
+i=1
+while [[ $i < 5 ]]
+do
+	echo $i
+	((i=i+1))
+done
 # ===end code===
 
