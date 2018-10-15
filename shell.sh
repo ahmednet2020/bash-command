4af565f0c3f376942862c37db6ad55dca5c1f816
ahmednet <ahmednet03@gmail.com>
2018-10-15 22:52:01 +0200

    filter file number

--- shell.sh
+++ shell.sh
@@ -4,10 +4,8 @@
 # === loop files in system
 ls -a | while read files; do
 	# check files name and find what you need
-	if [[ $files == *.js || $files == *.txt ]]; then
-		echo "${files}"
-	else
-		echo 'no this file'
+	if [[ $files == *.txt ]]; then
+		cut -d " " -f 4 $files
 	fi
 done
 # ===end code===
