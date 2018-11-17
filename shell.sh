59fc2d234b7d13e11c660fdd6e760107f0af4ff4
ahmednet <ahmednet03@gmail.com>
2018-11-17 20:43:23 +0200

    log file whith echo & read

--- shell.sh
+++ shell.sh
@@ -1,12 +1,20 @@
 #! /usr/bin/bash
 # ===start code===
 
-# === loop files in system
-ls -a | while read files; do
-	# check files name and find what you need
-	if [[ $files == *.txt ]]; then
-		cut -d " " -f 4 $files
-	fi
-done
+# === echo name
+echo -n "what's your name?   "
+read name
+echo -n "how old are you?   "
+read age
+echo -n "where are you live?   "
+read city
+
+if [[ $name == "ahmed" ]]; then
+	echo "welcome ahmed"
+	echo "you login in:" >> text.txt
+	date >> text.txt
+else
+	echo "you are not here"
+fi
 # ===end code===
 
