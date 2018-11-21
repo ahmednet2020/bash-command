ea4e08239d2939a1aa2a55e20e99c256f0231539
ahmednet <ahmednet03@gmail.com>
2018-11-21 22:12:59 +0200

    until loop

--- shell.sh
+++ shell.sh
@@ -1,20 +1,11 @@
 #! /usr/bin/bash
 # ===start code===
 
-# === echo name
-echo -n "what's your name?   "
-read name
-echo -n "how old are you?   "
-read age
-echo -n "where are you live?   "
-read city
-
-if [[ $name == "ahmed" ]]; then
-	echo "welcome ahmed"
-	echo "you login in:" >> text.txt
-	date >> text.txt
-else
-	echo "you are not here"
-fi
+# === until loop
+n=0;
+until (( $n > 10  )); do
+	echo $n;
+	((n++));
+done
 # ===end code===
 
