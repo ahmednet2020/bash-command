6f79fbb5a3ed781a074265944fd4c32ffccb76c5
ahmednet <ahmednet03@gmail.com>
2018-11-25 22:35:46 +0200

    for loop

--- shell.sh
+++ shell.sh
@@ -1,11 +1,22 @@
 #! /usr/bin/bash
 # ===start code===
 
-# === until loop
-n=0;
-until (( $n > 10  )); do
-	echo $n;
-	((n++));
+# === for loop
+
+# 1- commend loop
+for commend in git\ status ls; do
+	echo "------$commend"
+	$commend
+done
+sleep 1
+clear
+# 2- normal loop
+for (( i = 0; i < 10; i++ )); do
+	echo $i
+done
+# 3- loop in file exper
+for item in *; do
+	echo $item
 done
 # ===end code===
 
