7e7185394fe540ab8c574daacbf246256c773ed1
ahmednet <ahmednet03@gmail.com>
2018-11-27 14:51:40 +0200

    change name of img

--- shell.sh
+++ shell.sh
@@ -1,22 +1,13 @@
 #! /usr/bin/bash
 # ===start code===
 
-# === for loop
-
-# 1- commend loop
-for commend in git\ status ls; do
-	echo "------$commend"
-	$commend
-done
-sleep 1
-clear
-# 2- normal loop
-for (( i = 0; i < 10; i++ )); do
-	echo $i
-done
-# 3- loop in file exper
-for item in *; do
-	echo $item
+# === change name of img
+i=1;
+for img in *.jpg
+do
+	echo "$img";
+	mv "$img" "$i.jpg"
+	(( i++ ))
 done
 # ===end code===
 
